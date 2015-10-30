07fec44dd8f6019ea2a06b8c02cbe07b6f5f7760
Levi Goltz <lgoltz@Levis-MacBook-Pro-3.local>
2015-10-29 22:48:44 -0700

    More transparent errors for shopify get.

--- routes/shopify.js
+++ routes/shopify.js
@@ -8,17 +8,30 @@ var STORE_NAME = 'levisproxytest';
 
 router.get('/get', function(req, res) {
 
-	var path = req.query.path;
+	var path
+		, url
+		;
+
+	path = req.query.path;
 
 	res.set({'Content-Type': 'application/json'});
 
-	request('https://' + API_KEY + ':' + PASSWD + '@' + STORE_NAME + '.myshopify.com' + path
+	url = 'https://' + API_KEY + ':' + PASSWD + '@' + STORE_NAME + '.myshopify.com' + path;
+
+	console.log('req: ' + url);
+
+	request(url
 		, function (error, response, body) {
 			if (!error && response.statusCode == 200) {
 				res.status(200).send(body);
+			} else {
+				console.log(error);
+				console.log(response.statusCode);
+				res.status(response.statusCode).send(body);
 			}
 		}
 	);
+
 });
 
 router.post('/post', function(req, res){
